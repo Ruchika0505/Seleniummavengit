de814a046cf0eb259588689a17b78f9dc00fa5ae
ruchika0505 <ruchikas2323@gmail.com>
2022-10-20 19:16:43 +0530

    This is first commit

---
+++ run.bat
@@ -0,0 +1,4 @@
+cd C:\Users\ruchika\eclipse-workspace\mavenjava2
+mvn clean
+mvn compile
+mvn test
---
+++ src/test/java/QAclick2/UTTest.java
@@ -0,0 +1,22 @@
+package QAclick2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class UTTest {
+
+	@Test
+	public void stratBrowser()
+	{
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://opensource-demo.orangehrmlive.com");
+
+	Assert.assertTrue(driver.getTitle().contains("Orange"),"Tittle does not match");
+	driver.quit();		
+	}
+}
